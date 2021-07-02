04499903bbba390a29892e4e28735545e91d29ef
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2021-07-01 23:30:44 -0400

    Assignment 1

---
+++ Basic Javascripts Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+
+var x= "HELLO" + "   WORLD   "
+document.write(x);
+document.write(3+3); //expression
+
+document.write("\" Be happy with a beautiful Life," + " Life is an adventure. \"")  //concatenate string
+var dad= " BHAVJEET ", mumma="SUNPREET", kid=" HARNAV "; 
+var kid= "HARNAV";
+var kid= blues.fontcolor("kid"); //assigning color
+document.write(kid);
+
---
+++ Basic Javascripts Projects/helloworld.js
@@ -0,0 +1,4 @@
+var greeting= 'Hello, World';
+console.log(greeting);
+
+ 
