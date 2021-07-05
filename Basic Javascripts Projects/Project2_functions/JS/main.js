702ac07ef85b66c77bda8c804506181430d9717b
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2021-07-05 14:51:25 -0400

    assignment submission 2

---
+++ Basic Javascripts Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+
+function concatenate() { // creating a function
+    var a= 'I LIKE';    // assigning string value
+        a += 'CODING now'; // concatenating
+        a += ',It is fun!'; // concatenating
+        document.getElementById("concatenate").innerHTML= a; //assigning variable a to the html elements with concatenate id
+}
