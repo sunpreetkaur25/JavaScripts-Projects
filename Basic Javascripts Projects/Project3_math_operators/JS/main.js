1094e60b7adb27a7f80c5dd94c1d0b42e500f3f4
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2021-07-05 21:25:20 -0400

    assignment 16

---
+++ Basic Javascripts Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,4 @@
+function operator_addition() { // creating a function
+    var addition= 100 + 100;    // addition
+        document.getElementById("Math").innerHTML= "100 + 100 =" + addition; // addition  for the html elements to call
+}
