6fef51be57453239fa3c08edc0dfe823705e10ee
sunpreetkaur25 <Sunpreet.kaur2588@gmail.com>
2021-07-07 18:47:23 -0400

    assignment submission 5

---
+++ Basic Javascripts Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,11 @@
+document.write(typeof "Word"); //dispaly the data type of variable
+document.write( 5>2 && 4>3);  //using operator AND
+document.write( 5>2 || 4<3);  //using operator OR
+X= 10;                        // assigning value to X
+Y=10;                          // assigning value to Y
+document.write(X===Y);         //using operator === 
+document.write(5==10);          //using operator ==
+document.write("10"+5);         // adding string to number
+function not_function() {         // not function
+    document.getElementById("NOT").innerHTML= !(20>10);
+}
